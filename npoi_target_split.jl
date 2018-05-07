92a49dd647c884c3df5ffdb85798144bc17f0825
Fabien Baron <baron@chara.gsu.edu>
2018-05-07 18:09:08 -0400

    Fixed NPOI targets

--- npoi_target_split.jl
+++ npoi_target_split.jl
@@ -4,7 +4,7 @@ filename = "HD48329_oidata.fits";
 targetname =  "FKV0254";
 master = OIFITS.load(filename);
 db = OIFITS.select(master, "OI_TARGET")[1];
-target_filter = find(db[:target].==targetname);
+target_filter = db[:target_id][find(db[:target].==targetname)];
 for i in target_filter
     run(`oifits-filter --clobber $filename $targetname-$i.oifits --target-id=$i`);
 end
