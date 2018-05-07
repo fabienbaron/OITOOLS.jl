1efeddd8ca686d3d11635a1a2fbcdae9db6f90d6
Fabien Baron <baron@chara.gsu.edu>
2018-05-07 18:17:23 -0400

    Further cleanup of NPOI import

--- npoi_target_split.jl
+++ npoi_target_split.jl
@@ -6,7 +6,10 @@ master = OIFITS.load(filename);
 db = OIFITS.select(master, "OI_TARGET")[1];
 target_filter = db[:target_id][find(db[:target].==targetname)];
 for i in target_filter
-    run(`oifits-filter --clobber $filename $targetname-$i.oifits --target-id=$i`);
+    run(`oifits-filter --clobber $filename $targetname-$i.oifits --target-id=$i --accept-flagged=0`);
 end
 allfiles = string.(targetname,"-",target_filter,".oifits")
+allfiles=allfiles[isfile.(allfiles)] # remove possible empty ones
+run(`rm $targetname.oifits`);
 run(`oifits-merge $targetname.oifits $allfiles`);
+run(`rm $allfiles`);
