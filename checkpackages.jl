55e92f4560a31d7c975699240dce5dd971afe66c
Fabien Baron <baron@chara.gsu.edu>
2018-05-08 12:09:43 -0400

    Fixed installation

--- checkpackages.jl
+++ checkpackages.jl
@@ -14,7 +14,7 @@ end
 if Pkg.installed("OIFITS")==nothing
     print_with_color(:red, "OIFITS package is not installed... Installing now\n");
     Pkg.add("OIFITS")
-    Pkg.checkout("OIFITS","master")
+    #Pkg.checkout("OIFITS","master")
     print_with_color(:red, "Adding fixes to OIFITS.jl for OIFITS v2\n");
     cp("./fixes/oifile.jl",string(Pkg.dir("OIFITS"),"/src/oifile.jl"),remove_destination=true)
 end
