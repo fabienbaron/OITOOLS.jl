a0d794e137d26ef29baedc7204440e5405682cc7
Fabien Baron <baron@chara.gsu.edu>
2018-05-08 14:24:25 -0400

    Cleanup of npoi example

--- example_npoi_target_filter.jl
+++ example_npoi_target_filter.jl
@@ -3,23 +3,8 @@ include("oitools.jl");
 
 filename = "HD48329_oidata.fits";
 targetname =  "FKV0254";
-
-# solution based on JSY
-#master = OIFITS.load(filename);
-#db = OIFITS.select(master, "OI_TARGET")[1];
-#target_filter = db[:target_id][find(db[:target].==targetname)]; # this has the target ids correspond to the star we want
-#for i in target_filter
-#    run(`oifits-filter --clobber $filename $targetname-$i.oifits --target-id=$i --accept-flagged=0 --snr-min=0.01`);
-#end
-#allfiles = string.(targetname,"-",target_filter,".oifits")
-#allfiles=allfiles[isfile.(allfiles)] # remove possible empty ones
-#run(`rm $targetname.oifits`);
-#run(`oifits-merge $targetname.oifits $allfiles`);
-#run(`rm $allfiles`);
-#data = (readoifits("$targetname.oifits"))[1,1]; # Finally read the data
-
-# New solution
 data = (readoifits_filter(filename, targetname=targetname))[1,1];
+
 # Then we need to filter bad data
 good = find(  (data.v2_err.>0) .& (data.v2_err.<1.0) .& (data.v2.>-0.2) .& (data.v2.<1.2) )
 data.v2 = data.v2[good]
