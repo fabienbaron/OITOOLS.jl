030d7fa3304c92e79d5cbedb5363f7cc30a1763b
Fabien Baron <baron@chara.gsu.edu>
2018-05-11 11:27:20 -0400

    Readoifits_filter now default -- corrected issue with binning, introduced nobin default

--- oitools.jl
+++ oitools.jl
@@ -1,6 +1,5 @@
 include("checkpackages.jl");
 include("readoifits.jl");
-include("readoifits_filter.jl"); # experimental !
 include("oichi2.jl");
 include("oiplot.jl");
 include("vis_functions_classic.jl");
