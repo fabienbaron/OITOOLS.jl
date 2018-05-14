9ad5f0ac04d811d5adc4dac27319f0ae589d8bfd
Fabien Baron <baron@chara.gsu.edu>
2018-05-14 13:45:20 -0400

    Added NaN test to v2 filtering in readoifits

--- example2.jl
+++ example2.jl
@@ -14,6 +14,8 @@ cvis = visibility_ldpow([8.0,0.1], data.v2_baseline);
 
 # Fit uniform disc and plot
 f_chi2, params, cvis_model = fit_model_v2(data, visibility_ud, [8.0]);# diameter is the parameter
+
+# Plot model vs data
 v2_model = cvis_to_v2(cvis_model, data.indx_v2);
 v2plot_modelvsdata(data.v2_baseline,data.v2,data.v2_err, v2_model,logplot=true);
 
