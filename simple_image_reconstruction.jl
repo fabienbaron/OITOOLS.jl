76684336a23341b5d0a729680fa2898225cffb01
Fabien Baron <baron@chara.gsu.edu>
2018-05-25 11:03:00 -0400

    Improved chi2 dft speed

--- simple_image_reconstruction.jl
+++ simple_image_reconstruction.jl
@@ -16,7 +16,8 @@ pixsize = 0.1
 nx = 128
 data = readoifits(oifitsfile)[1,1];
 dft = setup_dft(data.uv, nx, pixsize);
-#fft = setup_nfft(data, nx, pixsize);
+#xtrue= vec(read(FITS("2004true.fits")[1]));
+#fftplan = setup_nfft(data.uv, nx, pixsize);
 #initial image is a simple Gaussian
  x_start = Array{Float64}(nx, nx);
      for i=1:nx
@@ -27,4 +28,5 @@ dft = setup_dft(data.uv, nx, pixsize);
  x_start = vec(x_start)/sum(x_start);
 crit = (x,g)->chi2_centered_fg(x, g, dft, data);
 x_sol = OptimPack.vmlmb(crit, x_start, verb=true, lower=0, maxiter=80, blmvm=false);
+imdisp(x_sol)
 f = FITS("reconst.fits", "w"); write(f, reshape(x_sol,(nx,nx))); close(f);
