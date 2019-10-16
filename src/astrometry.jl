2ce2d6af7af32bbd484c9dceebc95111595d9796
Fabien Baron <baron@phy-astr.gsu.edu>
2019-10-16 17:15:11 -0400

    Corrected HA calculation

--- src/astrometry.jl
+++ src/astrometry.jl
@@ -70,8 +70,8 @@ jdn = jdn + ((hours.-12)/24)+(minutes/1440)+(seconds/86400)
 jd0 = jdn .-2451545.0
 #gmst = 6.697374558 + (0.06570982441908*(jd0-(hour/24.))) + (1.00273790935*hour) + 0.000026*(jd0/36525)^2
 t = jd0/36525.0
-H = 24110.54841 .+ 8640184.812866*t + 0.093104*(t.^2) - (6.2*10.0^(-6))*(t.^3)
-w = 1.00273790935 .+ (5.9*10.0^(-11)*t)
+H = 24110.54841 .+ 8640184.812866*t + 0.093104*t.^2 - 6.2E-6*t.^3
+w = 1.00273790935 .+ 5.9e-11*t
 tt = hours*3600 + minutes*60 + seconds
 gmst = H + w.*tt #seconds
 gmst = gmst/3600 #hours
@@ -84,8 +84,8 @@ lst_under = findall(lst.<0)
 lst_over = findall(lst.>24)
 lst[lst_under] .+= 24
 lst[lst_over] -= (24*floor.(lst[lst_over]/24))
-
 hour_angle = lst .-ra
+hour_angle[findall(hour_angle.<-12)] .+= 24; # had to add this, normal ???? FB
 return lst,hour_angle
 end
 
@@ -117,97 +117,97 @@ function mjd_to_utc(mjd)
     return secs,mins,hour,day,month,year
 end
 
-function dates_to_jd(dates::Union{Array{Any,2},Array{Float64,2}})
-    """
-    This function calculates and returns the hour angle for the desired object given a RA, time, and longitude
-    of observations.  The program assumes UTC but changing the timezone argument to "local" will adjust the
-    input times accordingly.
+# function dates_to_jd(dates::Union{Array{Any,2},Array{Float64,2}})
+#     """
+#     This function calculates and returns the hour angle for the desired object given a RA, time, and longitude
+#     of observations.  The program assumes UTC but changing the timezone argument to "local" will adjust the
+#     input times accordingly.
+#
+#     Arguments:
+#     Manual Inputs:
+#     * date [2018  3 5 21 13 56.7; 2018 3 5 21 14 12.7] day,month,year,hour,min,sec: Correction to UTC is done within code if necessary/dictated.
+#     * longitude: degree (in decimal form) value of the longitudinal location of the telescope.
+#     * ra: [h,m,s] array of the right ascension of the desired object.
+#
+#     Optional Inputs:
+#     * dst: ["yes","no"] whether daylight savings time needs to be accounted for.
+#     * ldir: ["W","E"] defines the positive direction for change in longitude (should be kept to W for best applicability).
+#     * timezone: ["UTC","local"] states whether the time inputs are UTC or not.  If not the code will adjust the times as needed with the given longitude.
+#
+#     Accuracy:
+#     * With preliminary testing the LST returned is accurate to within a few minutes when compared with other calculators (MORE TESTING AND QUANTITATIVE ERROR NEEDS TO BE ESTABLISHED).
+#     """
+#     if ldir == "W"
+#         alpha = -1.
+#     elseif ldir == "E"
+#         alpha = 1.
+#     end
+#     years=Int.(dates[:,1])
+#     months=Int.(dates[:,2])
+#     days = Int.(dates[:,3])
+#     hours=Int.(dates[:,4])
+#     minutes=Int.(dates[:,5])
+#     seconds=Float64.(dates[:,6])
+#     h_ad = alpha*longitude/15 #Measures the hours offset due to longitude
+#     if timezone != "UTC"
+#         if dst=="no"
+#             hours .-= h_ad
+#         elseif dst=="yes"
+#             h_ad += 1
+#             hours .-= h_ad
+#         end
+#         wrap_hours_over = findall(hours.>24)
+#         hours[wrap_hours_over] .-= 24
+#         days[wrap_hours_over] .+= 1
+#
+#         wrap_hours_under = findall(hours.<0)
+#         hours[wrap_hours_under] +=24
+#         days[wrap_hours_under] -= 1
+#     end
+#
+#     #Below: the code calculates first the Julian Date given the input time and then determines the GMST based
+#     #on this JD.  This is then converted to LST and finally to Local Hour Angle (LHA or HA).  The final result
+#     #is in terms of hours for both LST and HA.
+#     jdn = floor.((1461*(years .+4800 .+(months.-14)/12))/4+(367*(months .-2-12*((months.-14)/12)))/12-(3*((years .+4900 +(months.-14)/12)/100))/4 .+days.-32075)
+#     jdn = jdn + ((hours.-12)/24)+(minutes/1440)+(seconds/86400)
+#     jd0 = jdn .-2451545.0
+#     return jd0
+# end
 
-    Arguments:
-    Manual Inputs:
-    * date [2018  3 5 21 13 56.7; 2018 3 5 21 14 12.7] day,month,year,hour,min,sec: Correction to UTC is done within code if necessary/dictated.
-    * longitude: degree (in decimal form) value of the longitudinal location of the telescope.
-    * ra: [h,m,s] array of the right ascension of the desired object.
 
-    Optional Inputs:
-    * dst: ["yes","no"] whether daylight savings time needs to be accounted for.
-    * ldir: ["W","E"] defines the positive direction for change in longitude (should be kept to W for best applicability).
-    * timezone: ["UTC","local"] states whether the time inputs are UTC or not.  If not the code will adjust the times as needed with the given longitude.
-
-    Accuracy:
-    * With preliminary testing the LST returned is accurate to within a few minutes when compared with other calculators (MORE TESTING AND QUANTITATIVE ERROR NEEDS TO BE ESTABLISHED).
-    """
-    if ldir == "W"
-        alpha = -1.
-    elseif ldir == "E"
-        alpha = 1.
-    end
-    years=Int.(dates[:,1])
-    months=Int.(dates[:,2])
-    days = Int.(dates[:,3])
-    hours=Int.(dates[:,4])
-    minutes=Int.(dates[:,5])
-    seconds=Float64.(dates[:,6])
-    h_ad = alpha*longitude/15 #Measures the hours offset due to longitude
-    if timezone != "UTC"
-        if dst=="no"
-            hours .-= h_ad
-        elseif dst=="yes"
-            h_ad += 1
-            hours .-= h_ad
-        end
-        wrap_hours_over = findall(hours.>24)
-        hours[wrap_hours_over] .-= 24
-        days[wrap_hours_over] .+= 1
-
-        wrap_hours_under = findall(hours.<0)
-        hours[wrap_hours_under] +=24
-        days[wrap_hours_under] -= 1
-    end
-
-    #Below: the code calculates first the Julian Date given the input time and then determines the GMST based
-    #on this JD.  This is then converted to LST and finally to Local Hour Angle (LHA or HA).  The final result
-    #is in terms of hours for both LST and HA.
-    jdn = floor.((1461*(years .+4800 .+(months.-14)/12))/4+(367*(months .-2-12*((months.-14)/12)))/12-(3*((years .+4900 +(months.-14)/12)/100))/4 .+days.-32075)
-    jdn = jdn + ((hours.-12)/24)+(minutes/1440)+(seconds/86400)
-    jd0 = jdn .-2451545.0
-    return jd0
-end
-
-
-function jd_to_hour_angle(jd::Array{Float64,1},tel_longitude::Float64, obj_ra::Float64;dst="no",ldir="W",timezone="UTC")
-
-"""
-This function calculates and returns the hour angle for the desired object given the object RA, the JD time, and longitude
-of observations.
-Arguments:
-Manual Inputs:
-* longitude: degree (in decimal form) value of the longitudinal location of the telescope.
-* ra: [h,m,s] array of the right ascension of the desired object.
-Accuracy:
-* With preliminary testing the LST returned is accurate to within a few minutes when compared with other calculators (MORE TESTING AND QUANTITATIVE ERROR NEEDS TO BE ESTABLISHED).
-"""
-
-t = jd/36525.0 # TODO check that correct jd here (= rjd - 51545.0) ???
-# Greenwich Mean Sidereal Time at Oh UT (in seconds)
-H = 24110.54841 .+ 8640184.812866*t + 0.093104*t.^2 - 6.2e-6*t.^3
-w = 1.00273790935 .+ 5.9e-11*t  # what's this ???
-tt = hours*3600 + minutes*60 + seconds # what's this ???
-gmst = H + w.*tt #seconds
-# Greenwich Mean Sidereal Time in hours
-gmst = gmst/3600 #hours
-gmst_over = findall(gmst.>24)
-gmst[gmst_over] -= (24*floor.(gmst[gmst_over]/24))
-# Local Mean Sidereal Time in hours at 0h UT (longitude correction)
-lst = gmst .+ longitude/15
-lst_under = findall(lst.<0)
-lst_over = findall(lst.>24)
-lst[lst_under] .+= 24
-lst[lst_over] -= (24*floor.(lst[lst_over]/24))
-# HA of star at 0h UT on RJD
-hour_angle = lst .-obj_ra
-return lst, hour_angle
-end
+# function jd_to_hour_angle(jd::Array{Float64,1},tel_longitude::Float64, obj_ra::Float64;dst="no",ldir="W",timezone="UTC")
+#
+# """
+# This function calculates and returns the hour angle for the desired object given the object RA, the JD time, and longitude
+# of observations.
+# Arguments:
+# Manual Inputs:
+# * longitude: degree (in decimal form) value of the longitudinal location of the telescope.
+# * ra: [h,m,s] array of the right ascension of the desired object.
+# Accuracy:
+# * With preliminary testing the LST returned is accurate to within a few minutes when compared with other calculators (MORE TESTING AND QUANTITATIVE ERROR NEEDS TO BE ESTABLISHED).
+# """
+#
+# t = jd/36525.0 # TODO check that correct jd here (= rjd - 51545.0) ???
+# # Greenwich Mean Sidereal Time at Oh UT (in seconds)
+# H = 24110.54841 .+ 8640184.812866*t + 0.093104*t.^2 - 6.2e-6*t.^3
+# w = 1.00273790935 .+ 5.9e-11*t  # what's this ???
+# tt = hours*3600 + minutes*60 + seconds # what's this ???
+# gmst = H + w.*tt #seconds
+# # Greenwich Mean Sidereal Time in hours
+# gmst = gmst/3600 #hours
+# gmst_over = findall(gmst.>24)
+# gmst[gmst_over] -= (24*floor.(gmst[gmst_over]/24))
+# # Local Mean Sidereal Time in hours at 0h UT (longitude correction)
+# lst = gmst .+ longitude/15
+# lst_under = findall(lst.<0)
+# lst_over = findall(lst.>24)
+# lst[lst_under] .+= 24
+# lst[lst_over] -= (24*floor.(lst[lst_over]/24))
+# # HA of star at 0h UT on RJD
+# hour_angle = lst .-obj_ra
+# return lst, hour_angle
+# end
 
 
 function opd_limits(base, alt, az)
@@ -223,7 +223,7 @@ function alt_az(dec_deg,lat_deg, ha_hours) #returns alt, az in degrees
     # Simple version
     alt = asin.(sin(dec)*sin(lat).+cos(dec)*cos(lat)*cos.(ha))
     az = atan.((-cos(dec)*sin.(ha))./(sin(dec)*cos(lat).-cos(dec)*cos.(ha)*sin(lat)))
-    return alt*180/pi, az*180/pi
+    return alt*180/pi, mod.(az*180/pi.+360, 360)
 end
 
 function geometric_delay(l,h,δ,baselines)
