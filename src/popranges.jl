49686174c6059e6117cf646b8a2fdb80edfcf27a
Fabien Baron <baron@phy-astr.gsu.edu>
2019-10-16 10:00:21 -0400

    Fixed typo in simulate.jl

---
+++ src/popranges.jl
@@ -0,0 +1,40 @@
+# Based on telescopes.chara from Jun 2019 update
+pop_array= zeros(6,5)
+#S1
+pop_array[1,1]=   0.000
+pop_array[1,2]=  36.5639684
+pop_array[1,3]=  73.1289412
+pop_array[1,4]= 109.7249843
+pop_array[1,5]= 143.0358086
+#S2
+pop_array[2,1]= -36.5447103
+pop_array[2,2]=   0.000
+pop_array[2,3]=  36.5686106
+pop_array[2,4]=  73.1577372
+pop_array[2,5]= 106.4675474
+#E1
+pop_array[3,1]=   0.000
+pop_array[3,2]=  36.5856149
+pop_array[3,3]=  73.1206645
+pop_array[3,4]= 109.7081816
+pop_array[3,5]= 143.0198375
+#E2
+pop_array[4,1]= -73.1122149
+pop_array[4,2]= -36.5385324
+pop_array[4,3]=   0.000
+pop_array[4,4]=  36.578125261
+pop_array[4,5]=  69.922248720
+#W1
+pop_array[5,1]=  -73.1098438
+pop_array[5,2]=  -36.5368346
+pop_array[5,3]=    0.000
+pop_array[5,4]=   36.5936311
+pop_array[5,5]=   69.9061629
+#W2
+pop_array[6,1]= -143.055000
+pop_array[6,2]= -106.455324
+pop_array[6,3]= -69.911098
+pop_array[6,4]= -33.306356
+pop_array[6,5]=   0.000
+
+airpath = [ 0.000, 573633.833,  4250500.000, 3680300.000, 1842354.000, 2405131.000]*1E-6
