33c80eb680c497ec06bd983846dd0b82edb2f985
Fabien Baron <baron@astro.gsu.edu>
2020-10-30 11:36:39 -0400

    Fixed NaN when all data is missing from a given spectral channel

--- src/oichi2.jl
+++ src/oichi2.jl
@@ -555,7 +555,7 @@ function chi2_dft_fg(x::Array{Float64,1}, g::Array{Float64,1}, dft::Array{Comple
     g_v2 = 0.0
     g_t3amp = 0.0
     g_t3phi = 0.0
-    if (weights[1]>0)|(data.nv2==0)
+    if (weights[1]>0)||(data.nv2==0)
         chi2_v2 = norm((v2_model - data.v2)./data.v2_err)^2;
         g_v2 = real(transpose(dft[data.indx_v2,:])*(4*((v2_model-data.v2)./data.v2_err.^2).*conj(cvis_model[data.indx_v2])));
     end
@@ -598,17 +598,17 @@ function chi2_nfft_fg(x::Array{Float64,1}, g::Array{Float64,1}, ftplan::Array{NF
     g_v2 = 0.0
     g_t3amp = 0.0
     g_t3phi = 0.0
-    if weights[1]>0
+    if (weights[1]>0)||(data.nv2==0)
         chi2_v2 = norm((v2_model - data.v2)./data.v2_err)^2;
         g_v2 = real(nfft_adjoint(ftplan[3], (4*((v2_model-data.v2)./data.v2_err.^2).*cvis_model[data.indx_v2])));
     end
 
-    if weights[2]>0
+    if (weights[2]>0)||(data.nt3amp==0)
         chi2_t3amp = norm((t3amp_model - data.t3amp)./data.t3amp_err)^2;
         g_t3amp = real(nfft_adjoint(ftplan[4], (2.0*((t3amp_model-data.t3amp)./data.t3amp_err.^2).*cvis_model[data.indx_t3_1]./abs.(cvis_model[data.indx_t3_1]).*abs.(cvis_model[data.indx_t3_2]).*abs.(cvis_model[data.indx_t3_3]) ))) + real(nfft_adjoint(ftplan[5],(2.0*((t3amp_model-data.t3amp)./data.t3amp_err.^2).*cvis_model[data.indx_t3_2]./abs.(cvis_model[data.indx_t3_2]).*abs.(cvis_model[data.indx_t3_1]).*abs.(cvis_model[data.indx_t3_3]) ))) + real(nfft_adjoint(ftplan[6],(2.0*((t3amp_model-data.t3amp)./data.t3amp_err.^2).*cvis_model[data.indx_t3_3]./abs.(cvis_model[data.indx_t3_3]).*abs.(cvis_model[data.indx_t3_1]).*abs.(cvis_model[data.indx_t3_2]) )))
     end
     
-    if weights[3]>0
+    if (weights[3]>0)||(data.nt3phi==0)
         chi2_t3phi = norm(mod360(t3phi_model - data.t3phi)./data.t3phi_err)^2;
         g_t3phi = -360.0/pi*imag.(nfft_adjoint(ftplan[4], ((mod360(t3phi_model-data.t3phi)./data.t3phi_err.^2)./abs2.(t3_model)).*conj(cvis_model[data.indx_t3_2].*cvis_model[data.indx_t3_3]).*t3_model)+nfft_adjoint(ftplan[5], ((mod360(t3phi_model-data.t3phi)./data.t3phi_err.^2)./abs2.(t3_model)).*conj(cvis_model[data.indx_t3_1].*cvis_model[data.indx_t3_3]).*t3_model)+nfft_adjoint(ftplan[6], ((mod360(t3phi_model-data.t3phi)./data.t3phi_err.^2)./abs2.(t3_model)).*conj(cvis_model[data.indx_t3_1].*cvis_model[data.indx_t3_2]).*t3_model))
     end
