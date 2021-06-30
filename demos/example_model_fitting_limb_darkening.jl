bee536efafd53a7a5d24ddb80cdabdf01e0bbbcc
Fabien Baron <fbaron@gsu.edu>
2021-06-30 21:59:50 +0200

    Fixes to new example

--- demos/example_model_fitting_limb_darkening.jl
+++ demos/example_model_fitting_limb_darkening.jl
@@ -27,7 +27,7 @@ display(model);
 minf, minx, cvis_model, result = fit_model_nlopt(data, model, chi2_weights=[1.0,0,0]);
 
 # LsqFit: Levenberg-Marquardt (fast but inaccurate statistical uncertainties)
-minf, minx, cvis_model, result = fit_model_levenberg(data, model, chi2_weights=[1.0,0,0]);
+#minf, minx, cvis_model, result = fit_model_levenberg(data, model, chi2_weights=[1.0,0,0]);
 
 # UltraNest: Nested Sampling (best for statistical uncertainties)
 minf, minx, cvis_model, result = fit_model_ultranest(data, model, chi2_weights=[1.0,0,0]);
@@ -36,11 +36,12 @@ minf, minx, cvis_model, result = fit_model_ultranest(data, model, chi2_weights=[
 
 # How to compute complex visibilities
 # Here for Hestroffer power law with diameter = 8.0 and limb-darkening parameter 0.1
-dispatch_params([8.0], model);
+model = create_model(create_component(type="ldlin", name="Model"));
+dispatch_params([8.0,0.1], model);
 cvis = model_to_cvis(model, data)
 
-# How to compute chi2 for given model
-chi2v2 = model_to_chi2(data, model, chi2_weights=[1.0,0,0])
+# How to compute chi2 for given model (note: you don't need to compute cvis first)
+chi2v2 = model_to_chi2(data, model, [8.3,0.2], chi2_weights=[1.0,0,0])
 
 # Plot model vs data
 v2_model = cvis_to_v2(cvis_model, data.indx_v2);
