c9ad83e8a57ec93deac1ccd1edcd5ad260154ebf
Fabien Baron <fbaron@gsu.edu>
2021-07-06 11:06:53 +0200

    Improved example

--- demos/example_model_fitting_2004.jl
+++ demos/example_model_fitting_2004.jl
@@ -5,26 +5,25 @@ using OITOOLS
 data= readoifits("./data/2004-data1.oifits")[1,1];
 
 # Setup components for the 2004 Model
-c1 = create_component(type="ud", name="Star")
-
+c1 = create_component(type="ud", name="star")
+c1.spectrum_params[1].free = true; # we have two components, we just need to fit the flux of the first
 c1.vis_params[1].free = true; # diameter in mas
-c1.vis_params[1].maxval = 2.0; # diameter in mas
-c1.spectrum_params[1].free = true;
+c1.vis_params[1].maxval = 1.0; # maximum diameter in mas (we expect a barely reslved star)
 
-c2 = create_component(type="ring", name="pAGB-ring")
-c2.spectrum_params[1].free = true;
-# c2.vis_params[5].free = false
-# c2.vis_params[6].free = false
-# c2.vis_params[7].free = false
-# c2.vis_params[8].free = false
-# c2.vis_params[9].free = false
+c2 = create_component(type="ring", name="ring")
+c2.spectrum_params[1].free = false; # we have two components, we just need to fit the flux of the first
+c2.vis_params[5].free = false
+c2.vis_params[6].free = false
+c2.vis_params[7].free = false
+c2.vis_params[8].free = false
+c2.vis_params[9].free = false
 
 # Put the model together
 model = create_model(c1,c2)
 display(model) # tis shows all the model parameters and does a few tests
 
 
-minf, minx, cvis_model, result = fit_model_ultranest(data, model, min_num_live_points = 400, cluster_num_live_points = 100)
+minf, minx, cvis_model, result = fit_model_ultranest(data, model, min_num_live_points = 100, cluster_num_live_points = 50)
 # Smaller numbers -> faster convergence but worse posterior characterization
 # For optimization, min_num_live_points= 100-400 works fine
 # For logZ estimation, >1000 would typically be OK
