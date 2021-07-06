88b7fdba78a4d8fd6424456f3c3e6437e9ab4cdb
Fabien Baron <fbaron@gsu.edu>
2021-07-06 12:54:46 +0200

    Improved multicomponent fitting example

--- demos/example_model_fitting_2004.jl
+++ demos/example_model_fitting_2004.jl
@@ -19,7 +19,7 @@ c2.vis_params[8].free = false
 c2.vis_params[9].free = false
 
 # If one wants to fit the ring a little better, uncomment this
-# More free parameters, so much slower
+# More free parameters, so much much slower
 # c2.vis_params[5].free = true
 # c2.vis_params[6].free = true
 # c2.vis_params[7].free = true
