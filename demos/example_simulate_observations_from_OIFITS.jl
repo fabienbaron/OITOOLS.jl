e4c20b5687dc91a834ab4a5e5c7a14f016aed166
Fabien Baron <fbaron@gsu.edu>
2021-07-29 16:22:02 -0400

    Added example for simulation with model

--- demos/example_simulate_observations_from_OIFITS.jl
+++ demos/example_simulate_observations_from_OIFITS.jl
@@ -7,32 +7,11 @@ using OITOOLS
 image_file="./data/2004true.fits"
 pixsize=0.101
 
-oifitsin  = "./data/2004-data1.oifits"
-out_file  = "!./data/2004testsimulation.oifits"
+in_oifits  = "./data/2004-data1.oifits"
+out_oifits  = "./data/2004-simulated.oifits"
 
-simulate_from_oifits(oifitsin,out_file,image=image_file,pixsize=pixsize)
+simulate_from_oifits(in_oifits,out_oifits,image=image_file,pixsize=pixsize)
 
 #Compare simulated data to input
-
-#display the image
-fitsfile = "./data/2004true.fits";
-pixsize = 0.101; # in mas/pixel
-x_true = readfits(fitsfile); nx = (size(x_true))[1]; x_true=vec(x_true);
-imdisp(x_true, pixscale = pixsize, tickinterval = 1.0, beamsize = 1.0, beamlocation = [0.85, 0.85])
-
-#read the data file
-
-oifitsfile = "./data/2004testsimulation.oifits";
-data = (readoifits(oifitsfile))[1,1]; # data can be split by wavelength, time, etc.
-uvplot(data);
-v2plot(data,logplot=true);# Alternatively, one can do v2plot(data.v2_baseline,data.v2,data.v2_err,logplot=true);
-t3phiplot(data);
-
-# NFFT method
-ft = setup_nfft(data, nx, pixsize);
-# This computes the complete chi2
-f_chi2 = chi2_nfft_f(x_true, ft, data);
-# Compute |V|^2 observables and plot
-cvis_model = image_to_cvis_nfft(x_true, ft);
-v2_model = cvis_to_v2(cvis_model, data.indx_v2);
-v2plot_modelvsdata(data, v2_model);
+data1 = readoifits(in_oifits)
+data2 = readoifits(out_oifits)
---
+++ demos/example_simulate_observations_from_model.jl
@@ -0,0 +1,34 @@
+using OITOOLS
+using AstroTime
+#Simulate an observation using an analytic model of your target
+#In this example, observations start at UT 2018-08-13 at 3:00:00AM and last until 2018-08-13 8:30:00AM, with a period of 15 minutes
+dates = collect(UTCEpoch(2018,8,13,3,0,0.0):15minutes:UTCEpoch(2018,8,13,8,30,0.0))
+
+# Model info - here a simple limb-darkened disk
+model = create_model(create_component(type="ldlin", name="Model"));
+dispatch_params([3.0,0.15], model);
+
+# Target info
+target = read_obs_file("./data/default_obs.txt"); # read defaults (for OIFITS header)
+target.target[1] = "AZ Cyg"
+target.raep0[1] =  [20, 57, 59.4437981]'*[1.0, 1/60., 1/3600] # set ra
+target.decep0[1] = [46, 28, 00.5731825]'*[1.0, 1/60., 1/3600] # set dec
+
+
+# Simulation info
+facility    = read_facility_file("./data/CHARA_new.txt");
+combiner    = read_comb_file("./data/MIRC.txt");
+wavelength        = read_wave_file("./data/MIRC_LOWH.txt");
+v2m=1.0/100; v2a=1e-5; t3ampm=1.0/100; t3ampa=1e-6; t3phim=0.0; t3phia=0.5;
+errors      = define_errors(v2m,v2a,t3ampm,t3ampa,t3phim,t3phia);
+
+# Output file
+out_file="./data/simulation-limb_darkened_disk.oifits";
+
+simulate(facility, target, combiner, wavelength, dates, errors, out_file, model=model);
+
+#Check simulated data
+data = (readoifits(out_file))[1,1]; # data can be split by wavelength, time, etc.
+uvplot(data)
+uvplot(data, color="wav")
+uvplot(data, color="mjd")
