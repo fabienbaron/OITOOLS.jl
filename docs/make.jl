0076d918af653d7bbdb3d381541f45008b6a3f84
Benjamin Setterholm <setterholm@mpia.de>
2024-04-10 12:09:01 +0200

    Fix the documentation CI (maybe)

--- docs/make.jl
+++ docs/make.jl
@@ -5,22 +5,26 @@ ENV["PYTHON"] = ""
 Pkg.build("PyCall")
 using Documenter, OITOOLS
 
-makedocs(sitename = "OITOOLS",
+makedocs(;
+    modules=[OITOOLS],
+    sitename = "OITOOLS",
     doctest = false,
-    format = Documenter.HTML(
-    prettyurls = CI,
+    format = Documenter.HTML(;
+        prettyurls = CI,
     ),
     authors = "Fabien Baron and contributors",
-    pages = [ "Home" => "index.md",
-    "Installation" => "install.md",
-    "Examples" => Any[
-                "examples/intro.md",
-                "examples/reading.md",
-                "examples/plotting.md",
-                "examples/modeling.md",
-                "examples/simulating.md",
-                "examples/imaging.md"]
-                ]
+    pages = [
+        "Home" => "index.md",
+        "Installation" => "install.md",
+        "Examples" => [
+            "examples/intro.md",
+            "examples/reading.md",
+            "examples/plotting.md",
+            "examples/modeling.md",
+            "examples/simulating.md",
+            "examples/imaging.md",
+        ],
+    ],
 )
 
 
